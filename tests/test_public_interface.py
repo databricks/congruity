b2997465ca2473660031a72bd3b93805a6ed8c23
Martin Grund <martin.grund@databricks.com>
2024-05-17 16:22:41 +0200

    using list instead of generator in test

--- tests/test_public_interface.py
+++ tests/test_public_interface.py
@@ -35,6 +35,6 @@ def test_spark_context_parallelize(spark_session: "SparkSession"):
     )
     assert result == [20000, 100000, 3000]
 
-    val = spark_session.sparkContext.parallelize(range(0, 5))
+    val = spark_session.sparkContext.parallelize(list(range(0, 5)))
     assert val.count() == 5
     assert val.collect() == [0, 1, 2, 3, 4]
